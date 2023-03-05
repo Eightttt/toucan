cc97dd5cfc9a858dea1d2d180f5991844ec04a03
Angelico Teaño <angelico.teano@obf.ateneo.edu>
2023-03-05 23:00:09 +0800

    Configured design themes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:toucan/pages/landing.dart';
 
 void main() {
-  MaterialColor mainAppColor = const MaterialColor(0xfff28705, <int, Color> {
+  const MaterialColor mainAppColor = MaterialColor(0xfff28705, <int, Color> {
     50: Color(0xfff28705),
     100: Color(0xfff28705),
     200: Color(0xfff28705),
@@ -15,12 +15,46 @@ void main() {
     900: Color(0xfff28705),
   });
 
+  const Color toucanWhite = Color(0xFFFDFDF5);
+
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
       primarySwatch: mainAppColor,
-      scaffoldBackgroundColor: const Color(0xFFFDFDF5),
+      scaffoldBackgroundColor: toucanWhite,
       fontFamily: 'Inter',
+      iconTheme: const IconThemeData(color: mainAppColor),
+      bottomSheetTheme: const BottomSheetThemeData(
+        backgroundColor: toucanWhite,
+        elevation: 20,
+        modalBarrierColor: Colors.transparent,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(36))
+        ),
+      ),
+      inputDecorationTheme: const InputDecorationTheme(
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: mainAppColor),
+          borderRadius: BorderRadius.all(Radius.circular(17)),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        prefixIconColor: mainAppColor,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          minimumSize: const Size(double.infinity, 48),
+          elevation: 8,
+          textStyle: const TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 19,
+            letterSpacing: 0.5,
+          ),
+          foregroundColor: toucanWhite,
+        ),
+      ),
 
     ),
     initialRoute: '/',
