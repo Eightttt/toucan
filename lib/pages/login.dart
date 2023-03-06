fa8059280248382abc2bdd8503c8cc0c4276b7f9
Angelico Teaño <angelico.teano@obf.ateneo.edu>
2023-03-06 21:33:42 +0800

    Added form validation for login.dart

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -11,100 +11,125 @@ class LogIn extends StatefulWidget {
 
 // TODO: add Navigator.of(context).pop(context) after logging in
 class _LogInState extends State<LogIn> {
+  final formKey = GlobalKey<FormState>();
+
+  logInUser() {
+    final isValid = formKey.currentState?.validate();
+
+    if (isValid!) {
+      // TODO: save data
+      const snackBar = SnackBar(
+        content: Text("Submitted"),
+        backgroundColor: Colors.green,
+        duration: Duration(seconds: 2),
+      );
+      Navigator.of(context).pop(context);
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children:  [
-        // ======== EMAIL ========
-        Container(
-            margin: const EdgeInsets.fromLTRB(57, 37, 0, 3),
-            child: const Text(
-              'E-mail',
-              style: TextStyle(
-                color: Color(0xfff28705),
-                fontWeight: FontWeight.w600,
-              ),
-            )
-        ),
-        Container(
-          margin: const EdgeInsets.fromLTRB(47, 3, 47, 0),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              prefixIcon: Padding(
-                padding: EdgeInsets.fromLTRB(12, 0, 10, 0),
-                child: Icon(Icons.email_outlined, size: 21,),
-              ),
-            ),
-            validator: (value) {
-              if (value != null) {
-                return 'Field is empty';
-              } else {
-                return null;
-              }
-            },
-          ),
-        ),
-
-        // ======== PASSWORD ========
-        Container(
-            margin: const EdgeInsets.fromLTRB(57, 10, 0, 3),
-            child: const Text(
-              'Password',
-              style: TextStyle(
-                color: Color(0xfff28705),
-                fontWeight: FontWeight.w600,
-              ),
-            )
-        ),
-        Container(
-          margin: const EdgeInsets.fromLTRB(47, 3, 47, 0),
-          child: TextFormField(
-            obscureText: true,
-            decoration: const InputDecoration(
-              prefixIcon: Padding(
-                padding: EdgeInsets.fromLTRB(12, 0, 10, 0),
-                child: Icon(Icons.lock_outline_rounded, size: 21,),
-              ),
-            ),
-            validator: (value) {
-              if (value != null) {
-                return 'Field is empty';
-              } else {
-                return null;
-              }
-            },
-          ),
-        ),
-
-        // ======== FORGOT PASSWORD ========
-        Container(
-          margin: const EdgeInsets.fromLTRB(47, 0, 47, 0),
-          child: TextButton(
-            style: TextButton.styleFrom(
-              textStyle: const TextStyle(
-                fontSize: 12,
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:  [
+          // ======== EMAIL ========
+          Container(
+              margin: const EdgeInsets.fromLTRB(57, 37, 0, 3),
+              child: const Text(
+                'E-mail',
+                style: TextStyle(
+                  color: Color(0xfff28705),
+                  fontWeight: FontWeight.w600,
+                ),
               )
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(47, 3, 47, 0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                errorMaxLines: 3,
+                prefixIcon: Padding(
+                  padding: EdgeInsets.fromLTRB(12, 0, 10, 0),
+                  child: Icon(Icons.email_outlined, size: 21,),
+                ),
+              ),
+              validator: (value) {
+                const pattern = r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+                final regEx = RegExp(pattern);
+                if (!regEx.hasMatch(value!)) {
+                  return 'Invalid email';
+                } else {
+                  return null;
+                }
+              },
             ),
-              onPressed: () async {
-                // TODO: Add forgot password feature
-                Navigator.of(context).pop(context);
-              },
-              child: const Text('Forgot Password?')
           ),
-        ),
 
-        // ======== ENTER BUTTON ========
-        Container(
-          margin: const EdgeInsets.fromLTRB(47, 28, 47, 100),
-          child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop(context);
-              },
-              child: const Text('Enter')
+          // ======== PASSWORD ========
+          Container(
+              margin: const EdgeInsets.fromLTRB(57, 10, 0, 3),
+              child: const Text(
+                'Password',
+                style: TextStyle(
+                  color: Color(0xfff28705),
+                  fontWeight: FontWeight.w600,
+                ),
+              )
           ),
-        ),
-      ],
+          Container(
+            margin: const EdgeInsets.fromLTRB(47, 3, 47, 0),
+            child: TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                errorMaxLines: 3,
+                prefixIcon: Padding(
+                  padding: EdgeInsets.fromLTRB(12, 0, 10, 0),
+                  child: Icon(Icons.lock_outline_rounded, size: 21,),
+                ),
+              ),
+              validator: (value) {
+                const pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,}$";
+                final regEx = RegExp(pattern);
+
+                if (!regEx.hasMatch(value!)) {
+                  return 'Incorrect Password';
+                } else {
+                  return null;
+                }
+              },
+            ),
+          ),
+
+          // ======== FORGOT PASSWORD ========
+          Container(
+            margin: const EdgeInsets.fromLTRB(47, 0, 47, 0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(
+                  fontSize: 12,
+                )
+              ),
+                onPressed: () async {
+                  // TODO: Add forgot password feature
+                  Navigator.of(context).pop(context);
+                },
+                child: const Text('Forgot Password?')
+            ),
+          ),
+
+          // ======== ENTER BUTTON ========
+          Container(
+            margin: const EdgeInsets.fromLTRB(47, 28, 47, 0),
+            child: ElevatedButton(
+                onPressed: logInUser,
+                child: const Text('Enter')
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
