06aad81f1d2bb6e9b29e861584d2e0725e6609e7
Angelico Teaño <angelico.teano@obf.ateneo.edu>
2023-03-06 19:28:13 +0800

    Added form validation for signup

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -8,144 +8,180 @@ class SignUp extends StatefulWidget {
   State<SignUp> createState() => _SignUpState();
 }
 
-// TODO: add Navigator.of(context).pop(context) after signing up
 class _SignUpState extends State<SignUp> {
+  final formKey = GlobalKey<FormState>();
+
+  signUpUser() {
+    final isValid = formKey.currentState?.validate();
+
+    if (isValid!) {
+      // TODO: save data
+      const snackBar = SnackBar(
+        content: Text("Submitted"),
+        backgroundColor: Colors.green,
+        duration: Duration(seconds: 2),
+      );
+      Navigator.of(context).pop(context);
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else {
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children:  [
-        // ======== TITLE ========
-        Container(
-          alignment: Alignment.center,
-          margin: const EdgeInsets.fromLTRB(0, 63, 0, 63),
-          child: const Text(
-            'Set-up you Account',
-            style: TextStyle(
-            color: Color(0xfff28705),
-              fontWeight: FontWeight.w700,
-              fontSize: 25,
-            ),
-          )
-        ),
-
-        // ======== USERNAME ========
-        Container(
-          margin: const EdgeInsets.fromLTRB(57, 0, 0, 3),
-          child: const Text(
-            'Username',
-            style: TextStyle(
+    return Form(
+      key: formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:  [
+          // ======== TITLE ========
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.fromLTRB(0, 63, 0, 48),
+            child: const Text(
+              'Set-up you Account',
+              style: TextStyle(
               color: Color(0xfff28705),
-              fontWeight: FontWeight.w600,
+                fontWeight: FontWeight.w700,
+                fontSize: 25,
+              ),
+            )
+          ),
+
+          // ======== USERNAME ========
+          Container(
+            margin: const EdgeInsets.fromLTRB(57, 0, 0, 3),
+            child: const Text(
+              'Username',
+              style: TextStyle(
+                color: Color(0xfff28705),
+                fontWeight: FontWeight.w600,
+              ),
+            )
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(47, 3, 47, 0),
+            child: TextFormField(
+              maxLength: 16,
+              decoration: const InputDecoration(
+                errorMaxLines: 3,
+                prefixIcon: Padding(
+                  padding: EdgeInsets.fromLTRB(12, 0, 10, 0),
+                  child: Icon(Icons.person_outline_rounded, size: 21,),
+                ),
+                counterText: "",
+              ),
+              validator: (value) {
+                const pattern = r"^(?=[a-zA-Z0-9._]{4,20}$)(?!.*[_.]{2})[^_.].*[^_.]$";
+                final regEx = RegExp(pattern);
+
+                if (value!.length < 4 || value.length > 20) {
+                  return 'Must be within 4 to 20 alphanumeric characters, underscores, or periods';
+                } else if (!regEx.hasMatch(value)) {
+                  return 'Must not have leading, trailing, and repeating underscores and periods';
+                } else {
+                  return null;
+                }
+              },
+            ),
+          ),
+
+          // ======== EMAIL ========
+          Container(
+              margin: const EdgeInsets.fromLTRB(57, 10, 0, 3),
+              child: const Text(
+                'E-mail',
+                style: TextStyle(
+                  color: Color(0xfff28705),
+                  fontWeight: FontWeight.w600,
+                ),
+              )
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(47, 3, 47, 0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                errorMaxLines: 3,
+                prefixIcon: Padding(
+                  padding: EdgeInsets.fromLTRB(12, 0, 10, 0),
+                  child: Icon(Icons.email_outlined, size: 21,),
+                ),
+              ),
+              validator: (value) {
+                const pattern = r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+                final regEx = RegExp(pattern);
+                if (!regEx.hasMatch(value!)) {
+                  return 'Must be a valid email';
+                } else {
+                  return null;
+                }
+              },
+            ),
+          ),
+
+          // ======== PASSWORD ========
+          Container(
+              margin: const EdgeInsets.fromLTRB(57, 10, 0, 3),
+              child: const Text(
+                'Password',
+                style: TextStyle(
+                  color: Color(0xfff28705),
+                  fontWeight: FontWeight.w600,
+                ),
+              )
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(47, 3, 47, 34),
+            child: TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                errorMaxLines: 3,
+                prefixIcon: Padding(
+                  padding: EdgeInsets.fromLTRB(12, 0, 10, 0),
+                  child: Icon(Icons.lock_outline_rounded, size: 21,),
+                ),
+              ),
+              validator: (value) {
+                const pattern = r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])$";
+                final regEx = RegExp(pattern);
+
+                if (value!.length < 7) {
+                  return 'Must be at least 7 characters long';
+                } else if (!regEx.hasMatch(value)) {
+                  return 'Must have at least an uppercase, a lowercase, a number, and a special character';
+                } else {
+                  return null;
+                }
+              },
+            ),
+          ),
+
+          // ======== ENTER BUTTON ========
+          Container(
+            margin: const EdgeInsets.fromLTRB(47, 3, 47, 50),
+            child: ElevatedButton(
+                onPressed: signUpUser,
+                child: const Text('Enter')
+            ),
+          ),
+
+          // ======== ALREADY HAVE AN ACCOUNT ========
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.fromLTRB(47, 3, 47, 100),
+            child: TextButton(
+                onPressed: () async {
+                  Navigator.of(context).pop(context);
+                  await Future.delayed(const Duration(milliseconds: 200), widget.showLogInSheet
+                  );
+                },
+                child: const Text('Already have an account?')
             ),
           )
-        ),
-        Container(
-          margin: const EdgeInsets.fromLTRB(47, 3, 47, 0),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              prefixIcon: Padding(
-                padding: EdgeInsets.fromLTRB(12, 0, 10, 0),
-                child: Icon(Icons.person_outline_rounded, size: 21,),
-              ),
-            ),
-            validator: (value) {
-              if (value != null) {
-                return 'Field is empty';
-              } else {
-                return null;
-              }
-            },
-          ),
-        ),
-
-        // ======== EMAIL ========
-        Container(
-            margin: const EdgeInsets.fromLTRB(57, 10, 0, 3),
-            child: const Text(
-              'E-mail',
-              style: TextStyle(
-                color: Color(0xfff28705),
-                fontWeight: FontWeight.w600,
-              ),
-            )
-        ),
-        Container(
-          margin: const EdgeInsets.fromLTRB(47, 3, 47, 0),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              prefixIcon: Padding(
-                padding: EdgeInsets.fromLTRB(12, 0, 10, 0),
-                child: Icon(Icons.email_outlined, size: 21,),
-              ),
-            ),
-            validator: (value) {
-              if (value != null) {
-                return 'Field is empty';
-              } else {
-                return null;
-              }
-            },
-          ),
-        ),
-
-        // ======== PASSWORD ========
-        Container(
-            margin: const EdgeInsets.fromLTRB(57, 10, 0, 3),
-            child: const Text(
-              'Password',
-              style: TextStyle(
-                color: Color(0xfff28705),
-                fontWeight: FontWeight.w600,
-              ),
-            )
-        ),
-        Container(
-          margin: const EdgeInsets.fromLTRB(47, 3, 47, 34),
-          child: TextFormField(
-            obscureText: true,
-            decoration: const InputDecoration(
-              prefixIcon: Padding(
-                padding: EdgeInsets.fromLTRB(12, 0, 10, 0),
-                child: Icon(Icons.lock_outline_rounded, size: 21,),
-              ),
-            ),
-            validator: (value) {
-              if (value != null) {
-                return 'Field is empty';
-              } else {
-                return null;
-              }
-            },
-          ),
-        ),
-
-        // ======== ENTER BUTTON ========
-        Container(
-          margin: const EdgeInsets.fromLTRB(47, 3, 47, 50),
-          child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop(context);
-              },
-              child: const Text('Enter')
-          ),
-        ),
-
-        // ======== ALREADY HAVE AN ACCOUNT ========
-        Container(
-          alignment: Alignment.center,
-          margin: const EdgeInsets.fromLTRB(47, 3, 47, 150),
-          child: TextButton(
-              onPressed: () async {
-                Navigator.of(context).pop(context);
-                await Future.delayed(const Duration(milliseconds: 200), () => widget.showLogInSheet()
-                );
-              },
-              child: const Text('Already have an account?')
-          ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
