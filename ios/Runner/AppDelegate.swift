805b5d313a5969ee225757f854d7adec7e96a1f8
Angelico Teaño <angelico.teano@obf.ateneo.edu>
2023-04-13 20:21:06 +0800

    Add needed permissions

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Flutter
 
+import flutter_local_notifications
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -8,6 +10,13 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    FlutterLocalNotificationPlugin.setPluginRegistrantCallback{ (registry) in GeneratedPluginRegistrant.register(with: registry)}
+    
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
