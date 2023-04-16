351e6306e8e026e39dd977398fa23472e2014280
Angelico Teaño <angelico.teano@obf.ateneo.edu>
2023-04-17 00:32:40 +0800

    Change task field to title

--- lib/models/taskModel.dart
+++ lib/models/taskModel.dart
@@ -1,12 +1,12 @@
 class TaskModel {
   final DateTime _date;
-  final String _task;
+  final String _title;
   bool _isDone;
 
-  TaskModel(this._date, this._task, this._isDone);
+  TaskModel(this._date, this._title, this._isDone);
 
   DateTime get date => _date;
-  String get task => _task;
+  String get title => _title;
   bool get isDone => _isDone;
 
   set isDone(bool value) {
