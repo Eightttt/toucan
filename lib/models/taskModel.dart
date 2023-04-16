b153326d4dcc284003d7c6dc6f2e7b9f209a8dd7
Angelico Teaño <angelico.teano@obf.ateneo.edu>
2023-04-17 01:10:11 +0800

    Add id field to taskmodel

--- lib/models/taskModel.dart
+++ lib/models/taskModel.dart
@@ -1,10 +1,12 @@
 class TaskModel {
+  final String _id;
   final DateTime _date;
   final String _title;
   bool _isDone;
 
-  TaskModel(this._date, this._title, this._isDone);
+  TaskModel(this._id, this._date, this._title, this._isDone);
 
+  String get id => _id;
   DateTime get date => _date;
   String get title => _title;
   bool get isDone => _isDone;
