f796703386d888501b90104f7431467e68f5e8d5
Angelico Teaño <angelico.teano@obf.ateneo.edu>
2023-04-17 21:46:15 +0800

    Change order of fields in Task Model

--- lib/models/taskModel.dart
+++ lib/models/taskModel.dart
@@ -4,7 +4,7 @@ class TaskModel {
   final String _title;
   bool _isDone;
 
-  TaskModel(this._id, this._date, this._title, this._isDone);
+  TaskModel(this._id, this._title,  this._date, this._isDone);
 
   String get id => _id;
   DateTime get date => _date;
