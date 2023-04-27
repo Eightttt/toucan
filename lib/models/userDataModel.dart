7a1d1aa58ead9b2a541dcec12bbe425794edd8e7
Angelico Teaño <angelico.teano@obf.ateneo.edu>
2023-04-27 16:09:14 +0800

    Add followCode and followList to userDataModel

--- lib/models/userDataModel.dart
+++ lib/models/userDataModel.dart
@@ -2,14 +2,25 @@ import 'package:flutter/material.dart';
 
 class UserDataModel {
   final String _username;
+  final int _followCode;
+  final List<dynamic> _followingList;
   final String _greeter;
   final TimeOfDay _notificationTime;
   final String _urlProfilePhoto;
 
-  UserDataModel(this._username, this._greeter, this._notificationTime, this._urlProfilePhoto);
+  UserDataModel(this._username, this._followingList, this._followCode,
+      this._greeter, this._notificationTime, this._urlProfilePhoto);
 
   String get username {
     return _username;
+  }
+
+  int get followCode {
+    return _followCode;
+  }
+
+  List<dynamic> get followingList {
+    return _followingList;
   }
 
   String get greeter {
